8205d131c86350d28316214279df670872cd6cc3
cuong luong quoc <luongquoccuong2103@gmail.com>
2024-03-09 23:16:04 +0700

    first commit

---
+++ ncm/ncm/ncmApp.swift
@@ -0,0 +1,17 @@
+//
+//  ncmApp.swift
+//  ncm
+//
+//  Created by admin on 2/5/24.
+//
+
+import SwiftUI
+
+@main
+struct ncmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
